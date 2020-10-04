ca29eeb52f287304bf099a4e547b1d2a1d0355d0
Adrian Thamburaj <30349598+adrian-thumbtack@users.noreply.github.com>
2020-10-03 20:30:33 -0400

    haha

--- main.js
+++ main.js
@@ -1,19 +1,13 @@
 var x = -5;
-var y = -6;
 
 function on_click(){
 	x = $('#p_num').val();
-	window.location.href = "side.html";
-	y = $('#p_num').val();
 }
 
-function other_click(){
-	$('#temp').text(x + " " + y);
+function on_click2(){
+	$('#temp').text(x);
 }
 
-if ($('body#main').length > 0){
-	$('#button').click(on_click);
-}
-else if ($('body#side').length > 0){
-	$('#other_button').click(other_click);
-}
+$('#button').click(on_click);
+$('#button2').click(on_click2);
+
