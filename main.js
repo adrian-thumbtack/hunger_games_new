90259530647e61a9bcaaeb1eff8b00cd1b88f85b
Adrian Thamburaj <30349598+adrian-thumbtack@users.noreply.github.com>
2020-10-03 20:32:24 -0400

    yes

--- main.js
+++ main.js
@@ -4,10 +4,6 @@ function on_click(){
 	x = $('#p_num').val();
 }
 
-function on_click2(){
-	$('#temp').text(x);
-}
-
 $('#button').click(on_click);
-$('#button2').click(on_click2);
+
 
---
+++ other.js
@@ -0,0 +1,5 @@
+function on_click2(){
+	$('#temp').text(x);
+}
+
+$('#button2').click(on_click2);
