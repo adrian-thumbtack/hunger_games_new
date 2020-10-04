00c2781f6da8e94e973a2f280cec13f274e73382
Adrian Thamburaj <30349598+adrian-thumbtack@users.noreply.github.com>
2020-10-03 21:22:51 -0400

    oh god

--- main.js
+++ main.js
@@ -7,10 +7,11 @@ function on_click(){
 	}
 	else {
 		for (var i=0; i<num_tributes; i++){
-			$('<input/>').attr({
+			$('<tr><td>').append(
+				$('<input/>').attr({
 				"type": "text",
 				"class": "t_entry"
-			}).appendTo($("#tribute_entry"));
+			})).prependTo($("#tribute_entry"));
 		}
 		$("div#main").css("visibility", "hidden");
 		$("#tribute_entry").css("visibility", "visible");
